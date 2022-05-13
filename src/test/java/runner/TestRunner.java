31e28a2e8a51d31853fe44fb350288a3e5ee54c7
Olatunde Oladeni <olatundeoladeni@yahoo.com>
2022-05-13 13:49:27 +0100

    added test runner class

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,10 @@
+package runner;
+
+import com.intuit.karate.KarateOptions;
+import com.intuit.karate.junit4.Karate;
+import org.junit.runner.RunWith;
+
+@RunWith(Karate.class)
+@KarateOptions(features = "classpath:Features" )
+public class TestRunner {
+}
